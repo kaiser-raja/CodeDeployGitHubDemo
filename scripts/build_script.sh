5ebf3636a8c98948ee541dd17ae20c74dcd0d879
kaiser-raja <kraja@folio3.com>
2016-01-29 09:04:44 +0500

    Update

--- scripts/build_script.sh
+++ scripts/build_script.sh
@@ -3,5 +3,13 @@
 cd /opt/source/Backend
 
 mvn clean install -Dmaven.test.skip=true -q
+STATUS=$?
+    if [[ $STATUS -eq 0 ]];
+    then
+        echo "[+] Built successfully."
 
-mv /opt/source/Backend/target/Backend-0.1.0.war /opt/tomcat8/webapps/
+
+mv /opt/source/Backend/target/Backend-0.1.0.war /opt/tomcat8/webapps/Backend.war
+
+/opt/tomcat8/bin/shutdown.sh
+
